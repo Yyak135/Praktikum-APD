deaf9a58a439861c43203f3d52abab2aab4a49a9
yyak <anindtyapa@gmail.com>
2024-09-27 15:08:46 +0800

    Add files via upload

---
+++ kelasB1 2024/praktikum-4.py
@@ -0,0 +1,75 @@
+# tanpa perulangan
+# print("perulangan ke 0")
+# print("perulangan ke 1")
+# print("perulangan ke 2")
+# print("perulangan ke 3")
+# print("perulangan ke 4")
+# print("perulangan ke 5")
+# print("perulangan ke 6")
+# print("perulangan ke 7")
+# print("perulangan ke 8")
+# print("perulangan ke 9")
+
+# dengan perulanggan
+# batas = 20
+# for i in range(10, batas, 2) :
+#     print(f"Perulangan ke-{i}")
+
+# buah_banyak = ["apel", "mangga", "anggur" ]
+# for buah in buah_banyak :
+#     print(buah)
+
+# for baris in range(1, 4) : 
+#     print("baris ke", baris)
+#     for kolom in range(1, 4) :
+#         print(kolom, "kolom", end=" ")
+
+# while True :
+#     print("Hello")
+
+# lagi = "yes"
+# ulang = 0
+# while lagi == "yes" :
+#     ulang += 1
+#     print("mabar")
+#     lagi = input("mabar lagi ga?")
+# print("Seleai Mabar!")
+# print("perulangan terjadi sebanyak{ulang}kali")
+
+# pembatasan perulangan
+# for i in range(1, 10) :
+#     if i == 4 :
+#         break
+#     else :
+#         print(f"perulangan ke{i}")
+
+# while True :
+#      ulang = input("mabar lagi : ")
+#      if ulang.lower() == "gak" :
+#          break
+#      print("mabar lagi")
+
+# for i range (1, 10) :
+#     if i == 4 :
+#         continue
+# print(f"perulangan ke{i}")
+
+# for i in range(5) :
+#     for j in range (5) :
+#         print("*", end=" ")
+#     print()
+
+print("segitiga siku siku")
+for i in range(1, 6) :
+    print("*" * i)
+
+print("segitiga sama kaki")
+for i in range 
+
+    print("*" )
+
+
+kesempatan = 3
+
+while kesempatan > 0 :
+    
