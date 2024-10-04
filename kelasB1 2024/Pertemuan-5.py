6e60c5a58460858c1c4d21e104dce734ff85ab09
yyak <anindtyapa@gmail.com>
2024-10-04 15:20:21 +0800

    Add files via upload

---
+++ kelasB1 2024/Pertemuan-5.py
@@ -0,0 +1,85 @@
+# pertemuan 5
+# data = [1,2,3,4,True,[2,3],"adi"]
+# data_2 = list ((1,2,3,4,5))
+
+# data = ["apel","durian","jeruk"]
+# print(data)
+# print()
+
+# nilai_hapus_del = del data[2]
+# nilai_hapus = data.pop(1)
+# data.remove = "apel"
+# print(data)
+
+# data[0:2] = "anggur","semangka"
+# print(data)
+# print()
+# del data[2]
+# data.pop(1)
+# print(data)
+
+# print(len(data))
+# print()
+
+# data.extend(["rambutan","semangka"])
+# data.append("semangka")
+# data.append(['rambutan','semangka'])
+# print(data)
+# print()
+# data.insert(100,"rambutan")
+# print(data)
+
+
+# print(type(data[0]))
+# print(type(data_2))
+
+# data_mahasiswa = [
+#     ["060", "ifnu"],["APD", "ORSIKOM"]]
+#     ["065", "adi"]
+# ]
+# print(data_mahasiswa[0][2][0])
+
+# data_mahasiswa = [
+#     ["Admin", "admin123", "admin"], 
+#     ["user", "user1234", "user"]
+#     ]
+
+
+# buah = ["apel", "durian", "jeruk"]
+# index = 1
+# for data in buah :
+#     print(f"data ke {index}")
+#     print(data)
+#     print("="*10)
+
+# data_mahasiswa = [
+#     ["060", "ifnu"],["APD", "ORSIKOM"]
+#     ]
+    
+# for item, item in enumerate(data_mahasiswa) :
+#     print(f"data ke {index + 1}")
+#     print(f"username" : {item[0]})
+
+
+# tuple
+# buah = ["apel", "durian", "jeruk"]
+
+# buah1, *banyak_buah = buah
+# print(buah1)
+# print(banyak_buah)
+
+# data_mahasiswa = ["tya", "nou", "nay", "alya"]
+
+# print("Menu hapus data")
+# for i in range(len(data_mahasiswa)):
+#     print(f"data ke {i+1}")
+#     print(f"nama : {data_mahasiswa[i]}")
+# index_user = int(input("masukkan index yang ingin dihapus : "))
+# del_user = 
+
+
+
+
+
+
+
